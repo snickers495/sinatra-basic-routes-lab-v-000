7dcc8ac778b572808fa1e8e8618ca8a87a1b4231
Lisa Lee <snickers495@gmail.com>
2017-11-01 21:37:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    
+    "My name is "
   end
+
+  get '/hometown' do 
+    resp.status = 200
+    "My hometown is "
+  end 
 end
